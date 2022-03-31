289709c099a60b58135cb9eac388fbb0e230c0c1
web-developer130 <vikasnayakmmmut@gmail.com>
2022-03-31 17:56:32 +0530

    message

---
+++ function.js
@@ -0,0 +1,7 @@
+
+//anonymous function
+console.log(getmessage)
+var getmessage=function (){
+    return "welcome to home";
+}
+
---
+++ variable.js
@@ -0,0 +1,9 @@
+console.log(10)
+var a=10
+a="vikas"
+a=null
+a=undefined
+a=function(){}
+a={}
+a=[]
+console.log(a,typeof a)
